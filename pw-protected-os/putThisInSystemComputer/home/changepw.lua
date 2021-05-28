400afccbe5241a4b786f59a7af912311df4ba4ce
Scroogeee <30441849+Scroogeee@users.noreply.github.com>
2021-05-28 12:32:01 +0200

    Changed keychecks to event based programming

--- pw-protected-os/putThisInSystemComputer/home/changepw.lua
+++ pw-protected-os/putThisInSystemComputer/home/changepw.lua
@@ -1,14 +1,44 @@
+--get the filesystem
 local filesystem = require("filesystem")
+--get the component api for the data card (crypto)
 local component = require("component")
+
+--set keyfilepath and cmpfilepath
+local keyfilepath = "/key/keyfile.txt"
 local cmpfilepath = "/disk/keyhash.txt"
 
-print("new password:")
+--generate the new password
 local pw = require("uuid").next()
-print("The new pw is:")
-print(pw)
-pw = component.data.sha256(pw)
 
-local cmpfile = io.open(cmpfilepath,"w")
-io.output(cmpfile)
-io.write(pw)
-io.close(file)
+--define functions
+
+local function run()
+  print("The new pw is:")
+  print(pw)
+  --if there is a valid drive mounted under /key
+  if (filesystem.exists("/key")) then
+    --if there is an old keyfile, delete it
+    if (filesystem.exists(keyfilepath)) then
+      filesystem.remove(keyfilepath)
+    end
+    --write the new pw to the keyfile
+    local keyfile = io.open(keyfilepath, "w")
+    io.output(keyfile)
+    io.write(pw)
+    io.close(keyfile)
+  end
+  --compute the hash of the password
+  pw = component.data.sha256(pw)
+  --if there is an old cmpfile, delete it
+  if (filesystem.exists(cmpfilepath)) then
+    filesystem.remove(cmpfilepath)
+  end
+  --write the new hash to the cmpfile
+  local cmpfile = io.open(cmpfilepath, "w")
+  io.output(cmpfile)
+  io.write(pw)
+  io.close(cmpfile)
+end
+
+--execution
+run()
--- pw-protected-os/putThisInSystemComputer/home/keychecker.lua
+++ pw-protected-os/putThisInSystemComputer/home/keychecker.lua
@@ -1,55 +1,81 @@
 --get the filesystem
 local filesystem = require("filesystem")
---get the data card
+--get the component api for the data card (crypto)
 local component = require("component")
---get the computer reference
-local computer = require("computer")
+--get event
+local event = require("event")
+--get note for beeps
+local note = require("note")
+--get shell for clearing the screen
+local shell = require ("shell")
+
+--define other necessary variables
 
 local keyfilepath = "/key/keyfile.txt"
 local cmpfilepath = "/disk/keyhash.txt"
 
-local keyCorrect = false
+local keyCorrect = false;
 
-while(not(keyCorrect)) do
+--define functions
 
-print("waiting for key")
-local foundKey = false
+local function checkKey()
+  local keyfile = io.lines(keyfilepath)
 
-while (not(foundKey)) do
-os.sleep(5)
---check for a keyfile
-foundKey = filesystem.exists(keyfilepath)
+  local proposedkey = ""
+  for c in keyfile do
+    --append the file contents to make the proposed key
+    proposedkey = proposedkey..c
+  end
+
+  --hash the key
+  proposedkey = component.data.sha256(proposedkey)
+
+  local actualkeyhash = ""
+  local cmpfile = io.lines(cmpfilepath)
+
+  for c in cmpfile do
+    actualkeyhash = actualkeyhash..c
+  end
+
+  if (proposedkey == actualkeyhash) then
+    return true
+  else
+    shell.execute("cls")
+    print("Key incorrect!")
+    print("waiting for key...")
+    note.play("G4", 0.2)
+    note.play("Eb4", 0.2)
+    note.play("C4", 0.2)
+    return false
+  end
 end
 
-print("found keyfile")
-local keyfile=io.lines(keyfilepath)
+local function run()
+  --remove the welcome screen since no key was entered yet
+  shell.execute("cls")
+  --inform the user
+  print("waiting for key...")
+  while(not(keyCorrect)) do
+    --print("new component available:")
+    --print(address)
+    --check for a keyfile
+    local _, address, type = event.pull("component_added")
+    foundKey = filesystem.exists(keyfilepath)
+    if (foundKey) then
+      print("found keyfile")
+      keyCorrect = checkKey()
+    end
+  end
 
-local proposedkey = ""
-for c in keyfile do
---append the file contents to make the proposed key
-proposedkey=proposedkey..c
+  print("Correct Key!")
+  note.play("C4", 0.1)
+  note.play("E4", 0.1)
+  note.play("G4", 0.1)
+  note.play("C5", 0.2)
 end
 
---hash the key
-proposedkey = component.data.sha256(proposedkey)
-
-local actualkeyhash=""
-local cmpfile = io.lines(cmpfilepath)
-
-for c in cmpfile do
-actualkeyhash = actualkeyhash..c
-end
-
-if (proposedkey==actualkeyhash) then
-keyCorrect = true
-else
-print("Incorrect!")
-computer.beep(391.995,0.5)
-computer.beep(311.127,0.5)
-computer.beep(261.626,0.5)
-os.sleep(5)
-end
-
-end
-
-print("Correct Key!")
+--execution
+run()
+--put up the welcome screen
+shell.execute("cls")
+shell.execute("/etc/motd")
